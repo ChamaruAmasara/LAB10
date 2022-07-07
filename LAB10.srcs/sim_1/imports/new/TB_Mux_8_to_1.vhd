f9a444e42dfa700cf513feff429579d8091449e4
Ginushmal <60034915+Ginushmal@users.noreply.github.com>
2022-07-07 14:44:26 +0530

    updated MUX

--- LAB10.srcs/sim_1/imports/new/TB_Mux_8_to_1.vhd
+++ LAB10.srcs/sim_1/imports/new/TB_Mux_8_to_1.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 06/06/2022 12:01:25 AM
+-- Create Date: 06/02/2022 03:33:47 PM
 -- Design Name: 
--- Module Name: TB_Mux_8_to_1 - Behavioral
+-- Module Name: TB_MUX_8_to_1 - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,13 +31,51 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity TB_Mux_8_to_1 is
+entity TB_MUX_8_to_1 is
 --  Port ( );
-end TB_Mux_8_to_1;
+end TB_MUX_8_to_1;
 
-architecture Behavioral of TB_Mux_8_to_1 is
+architecture Behavioral of TB_MUX_8_to_1 is
+component Mux_8_to_1
+    Port ( S : in STD_LOGIC_VECTOR (2 downto 0);
+           D : in STD_LOGIC_VECTOR (7 downto 0);
+           EN : in STD_LOGIC;
+           Y : out STD_LOGIC);
 
+end component;
+signal D: STD_LOGIC_VECTOR (7 downto 0);
+signal S: STD_LOGIC_VECTOR (2 downto 0);
+signal EN: STD_LOGIC;
+signal Y:STD_LOGIC;
 begin
 
+UUT: Mux_8_to_1
+    PORT MAP
+    (
+    D =>D,
+    S=>S,
+    EN=>EN,
+    Y=>Y
+    );
+    
+    process
+    begin
+    EN <='1';
+    D<="01010101";
+    S<="110";
+    
+    wait for 20ns;
+    S<="011";
+    
+    wait for 20ns;    
+    S<="000";
+    
+    wait for 20ns;
+    S<="001";
 
+    wait;
+    
+    end process;
+       
 end Behavioral;
+
