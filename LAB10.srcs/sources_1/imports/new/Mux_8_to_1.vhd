3020284cc3b2a6a0f476070ffbd7c61b0dbe5aa7
DilshanRakshitha <88279947+DilshanRakshitha@users.noreply.github.com>
2022-07-07 14:34:59 +0530

    Added mux

---
+++ LAB10.srcs/sources_1/imports/new/Mux_8_to_1.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/05/2022 11:24:06 PM
+-- Design Name: 
+-- Module Name: Mux_8_to_1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Mux_8_to_1 is
+    Port ( D : in STD_LOGIC_VECTOR (7 downto 0);
+           EN : in STD_LOGIC;
+           S : in STD_LOGIC_VECTOR (2 downto 0);
+           Y : out STD_LOGIC);
+end Mux_8_to_1;
+
+architecture Behavioral of Mux_8_to_1 is
+component Decoder_3_to_8
+    port(
+               I : in STD_LOGIC_VECTOR (2 downto 0);
+               EN : in STD_LOGIC;
+               Y : out STD_LOGIC_VECTOR (7 downto 0)
+    );
+end component;
+signal DY : std_logic_vector(7 downto 0);
+begin
+    Decoder_3_to_8_0 : Decoder_3_to_8
+        port map(
+            I => S,
+            En => En,
+            Y => DY
+        );
+        
+    Y <= En AND (  (D(0) AND DY(0))
+                OR (D(1) AND DY(1))
+                OR (D(2) AND DY(2))
+                OR (D(3) AND DY(3))
+                OR (D(4) AND DY(4))
+                OR (D(5) AND DY(5))
+                OR (D(6) AND DY(6))
+                OR (D(7) AND DY(7)));
+end Behavioral;
